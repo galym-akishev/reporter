31aa5ba61973138b8cbf024a979585cd033dd3af
Galym Akishev <galymakishevkz@gmail.com>
2023-11-24 10:26:52 +0600

    Initial code for the project on bitrix

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Reporter',
+	'description' => 'Reporters website',
+	'keywords' => 'репортер, новости',
+	'robots' => 'index, follow'
+);
+?>
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('Главная');
+?>
 
-echo "Hello world!!!";
+<?php
+echo "Hi world!!!";
+?>
+
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
+
+
