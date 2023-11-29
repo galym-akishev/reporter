8805618b2f0213961734d459cbdc85f68ebddb5d
Galym Akishev <galymakishevkz@gmail.com>
2023-11-29 17:20:19 +0600

    Policy section done

---
+++ local/templates/main/components/bitrix/news.detail/privacy-policy/template.php
@@ -0,0 +1,29 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+<main>
+    <section class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto mb-5 mb-lg-0">
+                    <h1 class="mb-4"><?= $arResult["NAME"] ?></h1>
+                    <div class="content">
+                        <?= $arResult["DETAIL_TEXT"] ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
